9e3fa60efd0f1332d763d5ca3fe855bb1fdb2db1
birenyadav <byadav@masaglobal.com>
2022-05-06 12:46:20 +0530

    first commit

---
+++ contracts/pract.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier:UNLICENSED
+
+
+pragma solidity 0.8.13;
+
+contract practise {
+uint number ;
+ function set(uint _number) public {
+     number = _number;
+ }
+function get() public view returns (uint){
+    return number;
+}
+
+}
+
+
